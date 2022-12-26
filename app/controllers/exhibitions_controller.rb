19175e4d0de3b5849863786d9e3e96f25eb1b751
関目智史 <zou3road@gmail.com>
2022-12-26 16:18:35 +0900

    Add:faker_index_renderOK

--- app/controllers/exhibitions_controller.rb
+++ app/controllers/exhibitions_controller.rb
@@ -1,4 +1,6 @@
 class ExhibitionsController < ApplicationController
+  skip_before_action :require_login, only: %i[index]
+  
   def index
     @exhibitions = Exhibition.all.includes(:user).order(created_at: :desc)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 10.times do
+#     User.create(
+#         name: Faker::Name.name,
+#         email: Faker::Internet.email,
+#         password: '12345678',
+#         password_confirmation: '12345678'
+#     )
+#   end
+  
+  20.times do |index|
+    Exhibition.create(
+        user: User.offset(rand(User.count)).first,
+        title: "個展#{index}",
+        description: "個展の紹介だよ#{index}"
+    )
+  end
+  p "OK"
