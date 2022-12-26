ef49bf627bd41f642d9a1fbff3d7228900eeb65a
関目智史 <zou3road@gmail.com>
2022-12-26 14:39:35 +0900

    Fix:delete_enumカラム

--- db/migrate/20221226043150_create_exhibitions.rb
+++ db/migrate/20221226043150_create_exhibitions.rb
@@ -4,8 +4,7 @@ class CreateExhibitions < ActiveRecord::Migration[6.1]
       t.string :title, null: false
       t.string :description
       t.references :user, null: false, foreign_key: true
-      t.integer :status,default: 0, null: false, limit: 1
-
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_25_135156) do
+ActiveRecord::Schema.define(version: 2022_12_26_043150) do
+
+  create_table "exhibitions", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_exhibitions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -22,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_12_25_135156) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "exhibitions", "users"
 end
