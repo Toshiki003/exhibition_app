5178207cd002896f649c4bdfae0ed1dc4a8fb86c
関目智史 <zou3road@gmail.com>
2022-12-29 10:18:49 +0900

    Fix:ユーザー登録後ログイン画面へ遷移する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_path, success: 'ユーザー登録が成功しました'
+      redirect_to login_path, success: 'ユーザー登録が成功しました'
     else
       flash.now[:danger] = "ユーザー登録に失敗しました"
       render :new
