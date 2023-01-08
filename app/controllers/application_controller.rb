66526bf60cd12cfa4d63b9e8306b76665704368b
関目智史 <zou3road@gmail.com>
2023-01-08 15:15:51 +0900

    Database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
     add_flash_types :success, :info, :warning, :danger
     before_action :require_login
 
-
     private
 
     def not_authenticated
